7866059ab520f072614f6fc064ef28d5ae9822dc
flyaway <jolvmail@gmail.com>
2020-06-27 16:44:10 +0800

    FIX: 🐛 koa-demo

--- example/koa-demo/index.js
+++ example/koa-demo/index.js
@@ -1,38 +1,38 @@
-
-const { CtripApplloClient } = require('../../src/index');
-const Koa = require('koa');
-const app = new Koa();
+const { CtripApplloClient, withValue } = require('../../src/index')
+const Koa = require('koa')
+const app = new Koa()
 
 const apollo = new CtripApplloClient({
-    configServerUrl: 'http://106.54.227.205:8080',
-    appId: 'apolloclient',
-    configPath: './config/apolloConfig.json',
-    namespaceList: ['application', 'development.qa']
-});
-
-const withValue = apollo.withValue;
+  configServerUrl: 'http://106.54.227.205:8080',
+  appId: 'apolloclient',
+  configPath: './config/apolloConfig.json',
+  namespaceList: ['application', 'development.qa']
+})
 
 class User {
-    constructor () {
-        withValue(this, 'userId', { field: 'user.id:10071' });
-    }
+  constructor () {
+    withValue(this, 'userId', 'user.id:10071')
+  }
 }
 
-// 支持热更新
-const userSex = apollo.hotValue({ field: 'user.sex:man' });
+// 支持热更新`
+const userSex = apollo.hotValue('user.sex:man')
 
 const run = async () => {
-    await apollo.init();
-    const user = new User();
+  await apollo.init()
+  const port = apollo.getValue('app.port:3000')
 
-    app.use(async ctx => {
-        // 配置变更后，会自动同步
-        ctx.body = {
-            userId: user.userId,
-            name: user.userName,
-            sex: userSex.value
-        };
-    });
-    app.listen(3000);
-};
-run();
+  const user = new User()
+
+  app.use(async ctx => {
+    // 配置变更后，会自动同步
+    ctx.body = {
+      userId: user.userId,
+      name: user.userName,
+      sex: userSex.value
+    }
+  })
+  app.listen(port)
+  console.log('listening on port:', port)
+}
+run()
