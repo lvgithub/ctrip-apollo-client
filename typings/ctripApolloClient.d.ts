3046d2a410d3751ee01e7f59b0cda9942bcdbd5e
oldhu <me@oldhu.com>
2021-11-30 16:15:50 +0800

    add proper return type of functions

--- typings/ctripApolloClient.d.ts
+++ typings/ctripApolloClient.d.ts
@@ -16,16 +16,16 @@ export class value {
 }
 export declare class CtripApolloClient {
     constructor(option: Option)
-    static value(field: string, namespace?: string)
-    hotValue(field: string, namespace?: string)
-    static withValue(target: any, key: string, field: string, namespace?: string)
-    withValue(target: any, key: string, field: string, namespace?: string)
+    static value(field: string, namespace?: string): value
+    hotValue(field: string, namespace?: string): value
+    static withValue(target: any, key: string, field: string, namespace?: string): void
+    withValue(target: any, key: string, field: string, namespace?: string): void
     init(): Promise<any>
     getConfigs(): Promise<any>
     getValue(field: string, namespace?: string): string
     onChange(callback: (obj: any) => void): void
 }
 
-export function value(field: string, namespace?: string)
-export function withValue(target: any, key: string, field: string, namespace?: string)
+export function value(field: string, namespace?: string): value
+export function withValue(target: any, key: string, field: string, namespace?: string): void
 export function hotValue(field: string, namespace?: string): value
